7e05271a35df6a06482d958a6c783befe0cbe70c
Aaron <aaron.pollock@coinbase.com>
2022-01-27 18:18:16 -0800

    Updates

---
+++ precache-manifest.c23e0dd05d0eea0d30fa078dac999a5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f9c97f2a33b975968d5eb76715eb2a",
+    "url": "/connect/index.html"
+  },
+  {
+    "revision": "2e3b35329436b9c8485e",
+    "url": "/connect/static/js/2.be83a1a7.chunk.js"
+  },
+  {
+    "revision": "30a3a9572b9acc4833cff5adc8303c94",
+    "url": "/connect/static/js/2.be83a1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6996b27395eddb21ff",
+    "url": "/connect/static/js/main.7aafb2db.chunk.js"
+  },
+  {
+    "revision": "d88a8f6a812cc3a3f5e7",
+    "url": "/connect/static/js/runtime-main.b6267f8b.js"
+  }
+]);
